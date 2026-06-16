b0f5a5ae7590bf4d1225ffaaf7870e91495d1f87
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-06-16 21:38:00 +0530

    âœ… Solve CF 510A - Fox and Snake | Implementation

---
+++ Problem-510A.py
@@ -0,0 +1,13 @@
+n, m = list(map(int, input().split(" ")))
+right = True
+
+for i in range(n):
+    if i % 2 == 0:
+        print("#" * m)
+    else:
+        if right:
+            print("." * (m - 1) + "#")
+            right = False 
+        else:
+            print("#" + "." * (m - 1))
+            right = True
