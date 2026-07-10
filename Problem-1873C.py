794e93c0ca8ca69d64a437b011cc51e5a19da2dc
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-07-10 12:40:00 +0530

    ✅ Solve CF 1873C - Target Practice | Implementation, Math

---
+++ Problem-1873C.py
@@ -0,0 +1,32 @@
+t = int(input())
+arr = []
+
+arrow = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 3, 3, 3, 3, 3, 3, 2, 1],
+    [1, 2, 3, 4, 4, 4, 4, 3, 2, 1],
+    [1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
+    [1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
+    [1, 2, 3, 4, 4, 4, 4, 3, 2, 1],
+    [1, 2, 3, 3, 3, 3, 3, 3, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+for i in range(t):
+    throw = []
+    total = 0
+
+    for i in range(10):
+        throw.append(list(input()))
+
+    for a, b in zip(arrow, throw):
+        for x, y in zip(a, b):
+            if y == "X":
+                total += x
+
+    arr.append(total)
+
+for a in arr:
+    print(a)
